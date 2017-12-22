73e548c1a0d7253e7aa584d71ebcf5df4f011826
Dana Pellerin <dana@pellerin.com>
2017-12-22 09:50:48 -0800

    Report on interval instead of continuous

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
-console.log("starting");
-
 var Bleacon = require('bleacon');
-
-var uuid = 'e2c56db5dffb48d2b060d0f5a71096e0';
-var major = 0; // 0 - 65535
-var minor = 0; // 0 - 65535
-var measuredPower = -59; // -128 - 127 (measured RSSI at 1 meter)
+var checkpoint = new Date();
+var interval = 5; // Seconds
 var tiltred = 'a495bb10c5b14b44b5121370f02d74de'
 
 // Bleacon.startAdvertising(uuid, major, minor, measuredPower);
 
 Bleacon.on('discover', function(bleacon){
-    console.log("Tilt RED: " + bleacon.uuid);
-    console.log("Temp    : " + bleacon.major + "f");
-    console.log("SG      : " + (bleacon.minor/1000)); 
+    elapsedTime = (new Date().getTime() - checkpoint.getTime()) / 1000;
+    if (elapsedTime > interval) {
+        checkpoint = new Date();
+        console.log("Time    : " + checkpoint);
+        console.log("Tilt RED: " + bleacon.uuid);
+        console.log("Temp    : " + bleacon.major + "f");
+        console.log("SG      : " + (bleacon.minor/1000)); 
+    }
 });
 
 Bleacon.startScanning(tiltred);
+console.log("Listening for Tilt Red");
