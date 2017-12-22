083f23e94c50bfd8ed88b4775a9f77726595ce1a
Dana Pellerin <dana@pellerin.com>
2017-12-21 19:50:23 -0800

    Added Tilt Red UUID

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ var uuid = 'e2c56db5dffb48d2b060d0f5a71096e0';
 var major = 0; // 0 - 65535
 var minor = 0; // 0 - 65535
 var measuredPower = -59; // -128 - 127 (measured RSSI at 1 meter)
-var tiltred = 'A495BB10C5B14B44B5121370F02D74DE'
+var tiltred = 'a495bb10c5b14b44b5121370f02d74de'
 
-Bleacon.startAdvertising(uuid, major, minor, measuredPower);
+// Bleacon.startAdvertising(uuid, major, minor, measuredPower);
 
-// Bleacon.on('discover', function(bleacon){
-//     console.log(bleacon);
-// });
+Bleacon.on('discover', function(bleacon){
+    console.log(bleacon);
+});
 
-// Bleacon.startScanning();
+Bleacon.startScanning(tiltred);
